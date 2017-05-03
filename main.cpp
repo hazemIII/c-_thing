be1e88436c1eea7d81a6f8c144fbb1055227c1c9
hazemiii <hazem@localhost.localdomain>
2017-05-03 19:44:24 +0200

    working version

--- main.cpp
+++ main.cpp
@@ -38,6 +38,20 @@ bool InvertMatrix(const matrix<T>& input, matrix<T>& inverse)
 	return true;
 }
 
+void p (matrix <double>& vec , matrix<double>& mat , matrix<double>& res)
+{
+    cout << vec.size2() << "   "<<mat.size1() <<endl;
+    for (int i = 0; i < vec.size2();i++){
+        for (int j = 0 ; j < mat.size1() ; j++){
+        cout << vec(0,i) << "   "<< mat(j,i)<<"  "<<endl;
+            res(0,i) =res(0,i)+ (vec(0,i) * mat(j,i));
+            cout<<vec(0,i) * mat(j,i)<<endl;
+
+        }
+    }
+
+}
+
 void print (std::vector <double> MatrixA)
 {
 
@@ -56,18 +70,17 @@ void print (std::vector <double> MatrixA)
 
 int main()
 {
-    int row = 100;
     //int n =10;
     float sx = (2* M_PI)/n;
-    // create vector F :   F = -sinx
+   /* // create vector F :   F = -sinx
     std::vector <double> F(10,0);    // vector with 10 elements all = 0
-    F[10] = 2 * M_PI;           // the last element = 2* pi
+    F[10] = 0;           // the last element = 2* pi
     for(int i = 1 ; i < 10 ; i++)
         F[i] = -1* sin(sx * i) ; // fill the F vector
 
     // Create Uex vector
     std::vector<double> Uex(10,0);
-    Uex[10] = 2 * M_PI;
+    Uex[10] = 0;
     for (int i = 0 ; i<= 10 ; i++)
     Uex[i] = sin(sx * i);// fill the Uex vector
 
@@ -87,23 +100,32 @@ int main()
     {
         MatrixA[(i*(n+1))-1] = 1;
     }
-    //print(MatrixA);
-    matrix<double> A(10,10) , Ainv(10,10);
+    //print(MatrixA);*/
+    matrix<double> A(n,n) , Ainv(n,n);
+    //fill matrix A
     for (int i = 0; i < A.size1 (); ++ i)
         for (int j = 0; j < A.size2 (); ++ j)
-            A (i, j) = MatrixA[j + 10* i];
-    //cout << A<<endl;
-    InvertMatrix(A,Ainv);
+            A(i,j) = 0 ;                         // initialize
+            /*A (i, j) = MatrixA[j + 100* i];*/
+    for (int i = 0 ; i <(A.size1()); i++)
+        {
+            A(i,i) = -2 ;
+        }
+    for(int row = 0 ; row < (A.size1()-1) ; row ++)
+        A(row, row+1) = 1;
+    for(int row = 1 ; row < (A.size1()) ; row ++)
+        A(row, row-1) = 1;
+       InvertMatrix(A,Ainv);
     //cout << Ainv<<endl;
-    matrix<double> f(1,10);
+    matrix<double> f(1,n);
     f(0,0) = 0.0;
-    f(0,9) = 2 * M_PI;
-    for (int i = 1 ; i< 10 ; i++){
+    f(0,n-1) =0;
+    for (int i = 1 ; i< n-1 ; i++){
         f(0,i) = -1 * sin(sx*i);}
 
-    matrix<double> U(1,10);
+    matrix<double> U(1,n);
     U = prod(f,Ainv);
-
-    cout <<U;
+    //cout << f << endl;
+    cout<<U<<endl;
     return 0;
 }
